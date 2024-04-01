4d7394a6235e7c39c529f3bef6c1df95b8384473
Rafael Citario <contato.rafaelgomes@outlook.com>
2024-04-01 15:13:05 -0300

    -a

---
+++ database/migrations/20240401180901_create-transactions-table.ts
@@ -0,0 +1,19 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('transactions', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.decimal('amount', 10, 2).notNullable()
+    table.text('type').notNullable()
+    table.timestamp('createdAt').defaultTo(knex.fn.now())
+    table.timestamp('updatedAt').defaultTo(knex.fn.now())
+    table.uuid('sessionId').index()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table('transactions', (table) => {
+    table.dropColumns('sessionId')
+  })
+}
